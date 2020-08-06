f9dc8bc9561f0428e879d68c688a18c63df3be5a
CheezItMan <chris@adadevelopersacademy.org>
2020-08-05 19:16:55 -0700

    homemade state machine

--- src/01/index.js
+++ src/01/index.js
@@ -1,25 +1,57 @@
 const elBox = document.querySelector('#box');
 
+const machine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        click: 'active',
+      },
+    },
+    active: {
+      on: {
+        click: 'inactive',
+      },
+    },
+  },
+};
+
 // Pure function that returns the next state,
 // given the current state and sent event
-function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
-  }
+function transition (state, event) {
+  return machine.states[state]
+    ?.on?.[event] || state;
+
+  // switch (state) {
+  //   case 'active':
+  //     switch (event) {
+  //       case 'click':
+  //         return 'inactive'
+  //       default:
+  //         return state;
+  //     }
+  //   case 'inactive':
+  //     switch (event) {
+  //       case 'click':
+  //         return 'active'
+  //       default:
+  //         return state;
+  //     }
+  //   default:
+  //     return state;
+  // }
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = 'inactive';
 
-function send(event) {
+function send (event) {
   // Determine the next value of `currentState`
+  currentState = transition(currentState, event);
 
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
-  // send a click event
+  send('click');
 });
