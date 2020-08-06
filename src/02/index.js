9b549fdd7f81dfaf46bacfd90daab91ec30f3a0d
CheezItMan <chris@adadevelopersacademy.org>
2020-08-05 19:17:07 -0700

    initial xstate example

--- src/02/index.js
+++ src/02/index.js
@@ -1,20 +1,41 @@
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from 'xstate';
 
 const elBox = document.querySelector('#box');
 
 const machine = createMachine({
-  // Add your object machine definition here
+  initial: 'inactive',
+  states: {
+    active: {
+      on: {
+        click: 'inactive',
+      },
+    },
+    inactive: {
+      on: {
+        click: 'active',
+      },
+    },
+
+  }
 });
 
+const machineService = interpret(machine);
+machineService.onTransition((state) => {
+  console.log(`Transitioning ${ state }`);
+  console.log(state);
+  elBox.dataset.state = state.value;
+})
+
+machineService.start();
+
+
+
 // Change this to the initial state
-let currentState = undefined;
+let currentState = 'inactive';
+elBox.dataset.state = currentState;
 
-function send(event) {
-  // Determine and update the `currentState`
-
-  elBox.dataset.state = currentState.value;
-}
 
 elBox.addEventListener('click', () => {
   // Send a click event
+  machineService.send('click');
 });
