c3444a5323ee00c697d55cffe47a5ea4acdca3fe
CheezItMan <chris@adadevelopersacademy.org>
2020-08-05 19:16:46 -0700

    initial example

--- src/00/index.js
+++ src/00/index.js
@@ -2,16 +2,16 @@ import { createMachine } from 'xstate';
 
 const elOutput = document.querySelector('#output');
 
-function output(object) {
+function output (object) {
   elOutput.innerHTML = JSON.stringify(object, null, 2);
 }
 
 console.log('Welcome to the XState workshop!');
 
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
+  name: 'Chris McAnally',
+  company: 'Ada',
+  interests: ['biking', 'Teaching'],
 };
 
 output(user);
